28cc436f7898037ad2babca1c3e2cbac456b8b72
Yauhen Artsiukhou <jsirex@gmail.com>
2016-01-11 19:06:59 +0300

    Update skeleton

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'jsirex@gmail.com'
 license          'apache 2'
 description      'Installs mailcatcher using embedded ruby in chef'
 long_description 'Installs mailcatcher using embedded ruby in chef'
-issues_url       'https://github.com/jsirex/simple-mailcatcher-cookbook/issues'
-source_url       'https://github.com/jsirex/simple-mailcatcher-cookbook'
+issues_url       'https://github.com/jsirex/simple-mailcatcher-cookbook/issues' if respond_to?(:issues_url)
+source_url       'https://github.com/jsirex/simple-mailcatcher-cookbook' if respond_to?(:source_url)
 version          '0.1.0'
